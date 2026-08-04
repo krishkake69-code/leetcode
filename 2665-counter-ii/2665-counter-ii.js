c9be8c2a4048564efee6ad89148edae04ec6a8c8
Priyanshu Attri <krishkake69@gmail.com>
2026-08-04 21:19:38 +0530

    Time: 57 ms (20.24%), Space: 55.2 MB (98.01%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let current = init;
+    return {
+        increment: function(){ 
+            return current += 1;
+        },
+        reset: function() {
+            return current = init;
+        },
+        decrement: function(){
+            return current -= 1;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
